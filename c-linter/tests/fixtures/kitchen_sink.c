8237eb9fa3f1668edb2ef936bb8ff7a34bf3285e
Charles Zhao <charleszhao94@gmail.com>
2026-06-24 20:39:00 -0700

    Add c-linter: a C++17 style linter for C: tests

---
+++ c-linter/tests/fixtures/kitchen_sink.c
@@ -0,0 +1,7 @@
+int totalCount = 0;
+int extremely_long_variable_name_used_only_to_blow_past_the_eighty_character_limit_value = 1;
+int trailing_ws = 1;   
+if (totalCount == 5)
+{
+    totalCount = 6;
+}
